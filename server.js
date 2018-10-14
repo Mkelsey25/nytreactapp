90b1212960363262fcc842c827a38b708737ad63
Mkelsey <mkh13b@my.fsu.edu>
2018-10-14 14:25:05 -0400

    added dotenv dependency

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -5,20 +6,21 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const env = require("./.env");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-/*if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-*/
+
 app.use(routes);
 
 //connect to database
-mongoose.connect("mongodb://localhost/nytarticles").then(() => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytarticles").then(() => {
 console.log("Hey, I'm MongoDB. You're connected to me!");
 });
 
