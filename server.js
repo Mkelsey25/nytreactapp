6a16adfbf0cb385709f28aa9814d90d13dc156ab
Mkelsey <mkh13b@my.fsu.edu>
2018-10-16 19:52:30 -0400

    This verison works on localhost

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+}*/
 
 app.use(routes);
 
